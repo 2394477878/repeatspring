0bc564cff604275678b5f1b67cfc0e64f8a9e83d
2394477878 <w2394477878@163.com>
2019-09-19 19:50:27 +0800

    second

--- src/main/java/demo2/springboot/demo/Controller/helloController.java
+++ src/main/java/demo2/springboot/demo/Controller/helloController.java
@@ -1,15 +1,16 @@
 package demo2.springboot.demo.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class helloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name")String name, Model model){
-        model.addAttribute(name);
+    @RequestMapping("/")
+    public String index(){
+
         return "index";
     }
 }
